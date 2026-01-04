ca609a6df9be994f5056a80db3d72fa316bea18d
Sainava <sainava.modak@gmail.com>
2026-01-04 20:47:30 +0530

    Add Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ import numpy as np
 
 import dill
 
+from sklearn.metrics import r2_score
+
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -22,5 +24,41 @@ def save_object(file_path, obj):
 
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    """
+    Evaluate multiple machine learning models and return their R2 scores.
+
+    Args:
+        X_train (np.ndarray): Training features.
+        y_train (np.ndarray): Training target.
+        X_test (np.ndarray): Testing features.
+        y_test (np.ndarray): Testing target.
+        models (dict): A dictionary where keys are model names and values are model instances.
+
+    Returns:
+        dict: A dictionary with model names as keys and their R2 scores as values.
+    """
+    try:
+       report ={}
+
+       for i in range(len(list(models))):
+              model = list(models.values())[i]
+              model.fit(X_train, y_train)
+
+              y_train_pred = model.predict(X_train)
+    
+              y_test_pred = model.predict(X_test)
+
+              train_model_score = r2_score(y_train, y_train_pred)
+              test_model_score =r2_score(y_test, y_test_pred)
+    
+              report[list(models.keys())[i]] = test_model_score
+
+       return report
+
     except Exception as e:
         raise CustomException(e, sys)
